03d62b1a3b274f08346f84c2517b05c78fa9c1fe
Hamza Zahoor <zahoorhamza69@gmail.com>
2025-04-30 00:47:59 +0500

    shopping cards

--- home.php
+++ home.php
@@ -79,6 +79,90 @@ include 'components/connection.php';
             </div>
         </section> 
          <!-- home slider End -->
+          <section class='thumb'>
+                <div class="box-container">
+                    <div class="box">
+                        <img src="img/thumb2.jpg" alt="">
+                        <h3>Green Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore est culpa quo, dolore nam dignissimos architecto.</p>
+                        <i class='bx bx-chevron-right'></i>
+                    </div>
+                    <div class="box">
+                        <img src="img/thumb0.jpg" alt="">
+                        <h3>lemon Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore est culpa quo, dolore nam dignissimos architecto.</p>
+                        <i class='bx bx-chevron-right'></i>
+                    </div>
+                    <div class="box">
+                        <img src="img/thumb1.jpg" alt="">
+                        <h3>Green Cofee</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore est culpa quo, dolore nam dignissimos architecto.</p>
+                        <i class='bx bx-chevron-right'></i>
+                    </div>
+                    <div class="box">
+                        <img src="img/thumb.jpg" alt="">
+                        <h3>Green Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore est culpa quo, dolore nam dignissimos architecto.</p>
+                        <i class='bx bx-chevron-right'></i>
+                    </div>
+                </div>
+          </section>
+          <section class='container'>
+                <div class="box-container">
+                    <div class="box">
+                        <img src="img/about-us.jpg" alt="">
+                    </div>
+                    <div class="container">
+                        <div class="box">
+                            <img src="img/download.png" alt="">
+                            <span>healthy tea</span>
+                            <h1>save up to 50% off</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, temporibus.</p>
+                        </div>
+                    </div>
+                </div>
+          </section>
+          <section class='shop'>
+            <div class="title">
+                <img src="img/download.png" alt="">
+                <h1>Trending Products</h1>
+            </div>
+            <div class="row">
+                <img src="im/about.jpg" alt="">
+                <div class="row-detail">
+                    <img src="im/basil.jpg" alt="">
+                    <div class="top-footer">
+                        <h1>a cup of tea makes you healthy.</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="box-container">
+                <div class="box">
+                    <img src="img/card.jpg" alt="">
+                    <a href="view_products.php" class='btn'>shop now</a>
+                </div>
+                <div class="box">
+                    <img src="img/card0.jpg" alt="">
+                    <a href="view_products.php"class='btn'>shop now</a>
+                </div>
+                <div class="box">
+                    <img src="img/card1.jpg" alt="">
+                    <a href="view_products.php" class='btn'>shop now</a>
+                </div>
+                <div class="box">
+                    <img src="img/card2.jpg" alt="">
+                    <a href="view_products.php"class='btn'>shop now</a>
+                </div>
+                <div class="box">
+                    <img src="img/10.jpg" alt="">
+                    <a href="view_products.php"class='btn'>shop now</a>
+                </div>
+                <div class="box">
+                    <img src="img/6.webp" alt="">
+                    <a href="view_products.php" class='btn'>shop now</a>
+                </div>
+            </div>
+          </section>
          <?php include 'components/footer.php'; ?>
     </div>
    
