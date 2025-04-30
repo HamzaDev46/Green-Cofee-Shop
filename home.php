ffcdfcb41558c0ebb225c30c60e9c656cd052fe6
Hamza Zahoor <zahoorhamza69@gmail.com>
2025-05-01 00:21:41 +0500

    about +home ending

--- home.php
+++ home.php
@@ -128,9 +128,9 @@ include 'components/connection.php';
                 <h1>Trending Products</h1>
             </div>
             <div class="row">
-                <img src="im/about.jpg" alt="">
+                <img src="img/about.jpg" alt="">
                 <div class="row-detail">
-                    <img src="im/basil.jpg" alt="">
+                    <img src="img/basil.jpg" alt="" style="width: 100%; height:60%;">   
                     <div class="top-footer">
                         <h1>a cup of tea makes you healthy.</h1>
                     </div>
@@ -162,6 +162,77 @@ include 'components/connection.php';
                     <a href="view_products.php" class='btn'>shop now</a>
                 </div>
             </div>
+          </section>
+          <section class='shop-category'>
+            <div class="box-container">
+                <div class="box">
+                    <img src="img/6.jpg" alt="">
+                    <div class="detail">
+                        <span>BIG OFFERS</span>
+                        <h1>Extra 15% Off</h1>
+                        <a href="view_products.php" class='btn'>shop now</a>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="img/7.jpg" alt="">
+                    <div class="detail">
+                        <span>new in taste</span>
+                        <h1>cofee house</h1>
+                        <a href="view_products.php" class='btn'>shop now</a>
+                    </div>
+                </div>
+            </div>
+          </section>
+          <section class='services'>
+            <div class="box-container">
+                <div class="box">
+                    <img src="img/icon2.png" alt="">
+                    <div class="detail">
+                        <h3>great savings</h3>
+                        <p>save big every order</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="img/icon1.png" alt="">
+                    <div class="detail">
+                        <h3>24*7 support</h3>
+                        <p>one-on-one sup</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="img/icon0.png" alt="">
+                    <div class="detail">
+                        <h3>gift vouchers</h3>
+                        <p>vouchers on every festivals</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="img/icon.png" alt="">
+                    <div class="detail">
+                        <h3>worldwide delivery </h3>
+                        <p>dropship delivery</p>
+                    </div>
+                </div>
+            </div>
+          </section>
+          <section class='brand'>
+                <div class="box-container">
+                        <div class="box">
+                            <img src="img/brand (1).jpg" alt="">
+                        </div>
+                        <div class="box">
+                            <img src="img/brand (2).jpg" alt="">
+                        </div>
+                        <div class="box">
+                            <img src="img/brand (3).jpg" alt="">
+                        </div>
+                        <div class="box">
+                            <img src="img/brand (4).jpg" alt="">
+                        </div>
+                        <div class="box">
+                            <img src="img/brand (5).jpg" alt="">
+                        </div>
+                </div>
           </section>
          <?php include 'components/footer.php'; ?>
     </div>
